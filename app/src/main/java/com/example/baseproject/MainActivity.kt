4665c683ca9995f3a96f38abe55ad4610f74b2d7
klkm0 <so.blau@duocuc.cl>
2025-12-10 18:00:42 -0300

    pt.3 - arreglando el main activity

--- app/src/main/java/com/example/baseproject/MainActivity.kt
+++ app/src/main/java/com/example/baseproject/MainActivity.kt
@@ -2,19 +2,15 @@ package com.example.baseproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.example.baseproject.view.AppNavigation
-import com.example.baseproject.ui.screens.WelcomeScreen
-import com.example.baseproject.ui.theme.BaseAndroidProjectTheme
-import com.example.baseproject.view.screens.PerfilScreen
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.baseproject.ui.theme.BaseAndroidProjectTheme
+import com.example.baseproject.viewmodel.PerfilViewModel
+import com.example.baseproject.view.screens.PerfilScreen
 
 
 class MainActivity : ComponentActivity() {
@@ -28,14 +24,6 @@ class MainActivity : ComponentActivity() {
                 ) {
                     AppNavigation()
                 }
-            }
-        }
-    }
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            BaseAndroidProjectTheme {
                 val viewModel: PerfilViewModel = viewModel()
                 PerfilScreen(viewModel)
             }
